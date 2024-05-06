8d9dc6ead2c8dea8a3721debb444a2fc48e1b00b
Alfred Lapkovsky <alfred.px@gmail.com>
2024-05-06 19:45:24 +0200

    Initial Commit

---
+++ MultimodalPlantClassifier/MultimodalPlantClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultimodalPlantClassifierApp.swift
+//  MultimodalPlantClassifier
+//
+//  Created by Alfred Lapkovsky on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultimodalPlantClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
